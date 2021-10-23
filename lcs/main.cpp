ab227edf30b7b7fec51589adaf870aaba33854ec
derek <harrison.derek.w@gmail.com>
2021-10-23 23:13:46 +0200

    remove number of ops, refactor

--- lcs/main.cpp
+++ lcs/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  lcs
 //
-//  Created by Derek Harrison on 23/10/2021.
+//  Created by mndx on 23/10/2021.
 //
 
 #include <iostream>
@@ -13,12 +13,7 @@ typedef struct memo_table {
     std::string str;
 } m_table;
 
-int tot_num_ops = 0;
-int num_ops_saved = 0;
-
 std::string lcs(std::string x, std::string y, m_table** memo_table) {
-    
-    tot_num_ops++;
     
     int length_x = (int) x.length();
     int length_y = (int) y.length();
@@ -27,10 +22,10 @@ std::string lcs(std::string x, std::string y, m_table** memo_table) {
     
     //Get results from memo table if available
     if(memo_table[length_x - 1][length_y - 1].is_set) {
-        num_ops_saved++;
         return memo_table[length_x - 1][length_y - 1].str;
     }
 
+    //Compute longest common subsequence of substring
     if(length_x > 0 && length_y > 0) {
         if(x[length_x - 1] != y[length_y - 1]) {
             std::string x_substr = x.substr(0, length_x - 1);
@@ -57,6 +52,7 @@ std::string lcs(std::string x, std::string y, m_table** memo_table) {
             result = result + lcs(x_substr, y_substr, memo_table);
         }
         
+        //Add results to memo table
         memo_table[length_x - 1][length_y - 1].is_set = true;
         memo_table[length_x - 1][length_y - 1].str = result;
     }
@@ -64,16 +60,12 @@ std::string lcs(std::string x, std::string y, m_table** memo_table) {
     return result;
 }
 
-int main(int argc, const char * argv[]) {
-    
-    std::string x = "ABCBDAB";
-    std::string y = "BDCABA";
-    std::string longest_cs;
-    
+std::string longest_common_subs(std::string x, std::string y) {
     int max_size = 0;
     int len1 = (int) x.length();
     int len2 = (int) y.length();
     
+    //Compute max length input strings
     if(len1 < len2) {
         max_size = len2;
     }
@@ -81,6 +73,7 @@ int main(int argc, const char * argv[]) {
         max_size = len1;
     }
     
+    //Initialize memo table
     m_table** memo_table = new m_table*[max_size];
     for(int i = 0; i < max_size; ++i) {
         memo_table[i] = new m_table[max_size];
@@ -92,11 +85,28 @@ int main(int argc, const char * argv[]) {
         }
     }
     
-    longest_cs = lcs(x, y, memo_table);
+    //Compute longest common subsequence
+    std::string longest_cs = lcs(x, y, memo_table);
     
-    std::cout << longest_cs << std::endl;
-    std::cout << "tot_num_ops: " << tot_num_ops << std::endl;
-    std::cout << "num_ops_saved: " << num_ops_saved << std::endl;
+    //Reverse results
+    std::string l_c_s = "";
+    int lcs_len = (int) longest_cs.length();
+    
+    for(int i = lcs_len - 1; i > -1; --i) {
+        l_c_s = l_c_s + longest_cs[i];
+    }
+    
+    return l_c_s;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    std::string x = "ABCBDABABA";
+    std::string y = "BDCABAABA";
+    
+    std::string lcs = longest_common_subs(x, y);
+    
+    std::cout << lcs << std::endl;
     std::cout << "done" << std::endl;
     
     return 0;
