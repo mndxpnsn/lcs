3a1baa47f5fc5ad8ee400629643d49536c2e4722
derek <harrison.derek.w@gmail.com>
2021-10-23 19:49:09 +0200

    Initial Commit

---
+++ lcs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lcs
+//
+//  Created by Derek Harrison on 23/10/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
